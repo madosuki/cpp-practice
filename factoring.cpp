09431e954e3e0fedefa3ae0a2f4b1b4b3d59177c
Kosuke <yurikasamaaikatsu@gmail.com>
2018-04-05 23:54:36 +0900

    Update factoring.cpp

--- factoring.cpp
+++ factoring.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <string>
 
+// 整形
 std::string shape(const std::string& str) {
     char pre;
     std::string r = "";
@@ -32,6 +33,7 @@ std::string shape(const std::string& str) {
     return r;
 }
 
+// 素因数分解関数
 std::string factoring(const int &num, std::array<int, 10> prime, const int& point, std::string r) {
     if((num % prime[point]) == 0) {
         int result = num / prime[point];
@@ -46,8 +48,12 @@ std::string factoring(const int &num, std::array<int, 10> prime, const int& poin
 }
 
 int main() {
+    // 素数コレクション。適当。
     constexpr std::array<int, 10> prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    
+    // 素因数分解したい数。
     constexpr int num = 600;
+    
     std::string result = factoring(num, prime, 0, "");
     std::string finalresult = shape(result);
     printf("素因数分解 整形前 %s\n", result.c_str());
